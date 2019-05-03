653b9ec31caa538932bf8f96564ddd8f8b5cef78
T Floyd Wright <tfwright@gmail.com>
2019-05-03 10:16:45 -0500

    Release 0.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Spur.MixProject do
     [
       app: :spur,
       description: "Activity tracking for Elixir apps",
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
