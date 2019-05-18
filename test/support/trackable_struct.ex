d4412a604a43e54a1aa6bd4214b5a5e54fd65628
T Floyd Wright <tfwright@gmail.com>
2019-05-18 16:10:42 -0500

    Fix warnings

--- test/support/trackable_struct.ex
+++ test/support/trackable_struct.ex
@@ -1,8 +1,6 @@
 defmodule SpurTest.TrackableStruct do
   use Ecto.Schema
 
-  import Ecto.Changeset
-
   schema "trackable_structs" do
     belongs_to :user, SpurTest.AppUser
 
@@ -15,7 +13,7 @@ defmodule SpurTest.TrackableStruct do
   defimpl Spur.Trackable, for: SpurTest.TrackableStruct do
     def actor(trackable_struct), do: "#{trackable_struct.user_id}"
     def object(trackable_struct), do: "TrackableStruct:#{trackable_struct.id}"
-    def target(trackable_struct), do: nil
+    def target(_trackable_struct), do: nil
     def audience(trackable_struct), do: Ecto.assoc(trackable_struct, :watchers)
   end
 end
