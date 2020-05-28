f19cba931c7ee629b01a2521c47a7257fb2f5218
T Floyd Wright <tfwright@gmail.com>
2020-05-28 08:55:59 -0500

    Bump version to 0.4.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Spur.MixProject do
     [
       app: :spur,
       description: "Activity tracking for Elixir apps",
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
